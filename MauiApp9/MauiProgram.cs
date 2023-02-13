ae7a1bd929518384a367821630f53fd86146143a
Aram <aram.margaryan@loyhutz.de>
2023-02-13 13:31:16 +0400

    Add project files.

---
+++ MauiApp9/MauiProgram.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Maui.Markup;
+using Material.Components.Maui.Extensions;
+using Material.Components.Maui.Tokens;
+using Microsoft.Extensions.Logging;
+
+namespace MauiApp9;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+                  .UseMauiApp<App>()
+            .UseMaterialComponents(
+                new List<string>
+                {
+                    "Roboto-Regular.ttf",
+                    "Roboto-Italic.ttf",
+                    "Roboto-Medium.ttf",
+                    "Roboto-MediumItalic.ttf",
+                    "Roboto-Bold.ttf",
+                    "Roboto-BoldItalic.ttf",
+                }
+            )
+            .UseMauiCommunityToolkitMarkup();
+
+    FontMapper.AddFont("OpenSans-Regular.ttf", "OpenSans");
+
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
